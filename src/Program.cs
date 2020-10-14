0ffe1bc6049abec7b2909aad62e49b66017be6e8
Tom Hollis <57675191+tomrhollis@users.noreply.github.com>
2020-10-14 13:20:43 -0600

    stop watchdog seeing stopbeforebackup as crash

--- src/Program.cs
+++ src/Program.cs
@@ -579,7 +579,9 @@ namespace Vellum
         {
             if (!_backupManager.Processing)
             {
+                if (RunConfig.Backups.StopBeforeBackup) _bdsWatchdog.Disable();
                 _backupManager.CreateWorldBackup(worldPath, tempWorldPath, false, true);
+                if (RunConfig.Backups.StopBeforeBackup) _bdsWatchdog.Enable();
             }
             else
             {
@@ -591,7 +593,9 @@ namespace Vellum
         {
             if (!_backupManager.Processing && !_renderManager.Processing)
             {
+                if (RunConfig.Backups.StopBeforeBackup) _bdsWatchdog.Disable();
                 _backupManager.CreateWorldBackup(worldPath, tempWorldPath, false, false);
+                if (RunConfig.Backups.StopBeforeBackup) _bdsWatchdog.Enable();
                 _renderManager.Start(tempWorldPath);
             }
             else
